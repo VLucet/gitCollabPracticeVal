8b8e52361891c75b1e4b2a874591dbaa0471d219
jessiewang1 <jae.wang@mail.utoronto.ca>
2023-12-06 10:39:57 -0500

    make pca

--- irisplot.R
+++ irisplot.R
@@ -4,3 +4,12 @@ data("iris")
 
 ggplot(aes(x=Sepal.Length,y=Sepal.Width,colour=Species),data=iris)+
   geom_point(alpha=0.5)
+
+# more customization
+ggplot(aes(x=Sepal.Length,y=Sepal.Width,colour=Species,shape=Species),data=iris)+
+  geom_point(alpha=0.8)+
+  stat_ellipse()
+
+iris.pca <- iris %>%
+  dplyr::select(1:4) %>%
+  prcomp()
