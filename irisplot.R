1dcc74afe3f94e9b9db306154114f78d2048e0d8
jessiewang1 <jae.wang@mail.utoronto.ca>
2023-12-06 10:53:11 -0500

    plot pca

--- irisplot.R
+++ irisplot.R
@@ -10,6 +10,10 @@ ggplot(aes(x=Sepal.Length,y=Sepal.Width,colour=Species,shape=Species),data=iris)
   geom_point(alpha=0.8)+
   stat_ellipse()
 
+# pca
 iris.pca <- iris %>%
   dplyr::select(1:4) %>%
   prcomp()
+
+# plot pca
+autoplot(iris.pca, loadings=TRUE, loadings.label=TRUE)
